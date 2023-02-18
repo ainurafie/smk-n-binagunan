83a90e14fb0da8b562b20a18aac4f9860aaf2f6f
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2023-02-18 23:33:16 +0700

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
+    <title>SMK NEGERI 1 BINANGUN</title>
+    <link rel="icon" href="../../assets/image/logo.png">
     @vite('resources/css/app.css')
 </head>
 
