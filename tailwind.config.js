ff365069f10004cb1a209f4a386b1ff0c63b7e9e
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2023-02-24 20:33:16 +0700

    instalation font outfi

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
     fontFamily: {
       "inter": ["Inter", "sans-serif"],
       "open-sans": ["Open Sans", "sans-serif"],
+      "outfit": ["Outfit", "sans-serif"],
   },
   },
   plugins: [],
