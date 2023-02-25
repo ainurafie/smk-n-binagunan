318cfe59bc03794b699248bd1d16f87325075015
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2023-02-25 19:59:51 +0700

    login and form alumni

---
+++ resources/views/Alumni/Form.blade.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Open+Sans:wght@400;500;600;700&display=swap"
+        rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
+    <title>Form Input Data Alumni</title>
+    <link rel="icon" href="../../assets/image/logo.png">
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-[#F7F7F7]">
+    <livewire:navbar.navbar2 />
+    <livewire:navbar.navbar />
+
+    <div class="lg:mt-40 mt-40 md:px-[120px] px-5">
+        <h1 class="text-[32px] font-semibold font-open-sans mb-[50px] pt-[50px]">Form Input Data Alumni</h1>
+        <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900">NIS</label>
+            <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        </div>
+        <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900">Nama Alumni</label>
+            <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        </div>
+        <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900">Tahun Masuk</label>
+            <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        </div>
+        <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900">Kompetensi</label>
+            <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        </div>
+        <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900">Tahun Lulus</label>
+            <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        </div>
+        <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900">Tempat Lahir</label>
+            <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        </div>
+        <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900">Tanggal Lahir</label>
+            <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        </div>
+        <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900">Jenis Kelamin</label>
+            <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        </div>
+        <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900">Alamat</label>
+            <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        </div>
+        <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
+            <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        </div>
+        <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900">No HP/WA</label>
+            <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        </div>
+        <div class="mb-10">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900">Status</label>
+            <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        </div>
+        <div class="flex justify-center">
+            <button class="rounded-[5px] text-white py-4 px-30" style="border-radius:5px; background-color: #2F73FE; padding-left:130px; padding-right:130px; margin-bottom:250px;" data-modal-toggle="sukses">Simpan</button>
+        </div>
+    </div>
+    <livewire:footer.footer />
+    <livewire:modal.sukses />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
+    <script src="https://code.iconify.design/iconify-icon/1.0.5/iconify-icon.min.js"></script>
+
+</body>
+
+</html>
